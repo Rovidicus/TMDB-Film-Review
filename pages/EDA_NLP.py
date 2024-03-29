5da15bd1fd1e34ebc9998b527d876f1cac8cc2fd
Luke Nathaniel Hobbs <rovidicus@hotmail.com>
2024-03-28 18:06:24 -0700

    Update EDA_NLP.py

--- pages/EDA_NLP.py
+++ pages/EDA_NLP.py
@@ -18,14 +18,16 @@ with open('config/filepaths.json') as f:
 
 st.title("Exploratory Data Analysis of TMDB Reviews")
 
+st.markdown("Here is a dataframe of the trained review data for our model. Click on any of the review text boxes to see the full review. Zero as a rating means negative, one means positive.")
 
 @st.cache_data
 def load_data(fpath):
     return joblib.load(fpath)
 
 df = load_data(FPATHS['data']['processed'])
-df.head(2)
+st.dataframe(df, width=1000, height=500)
 
+st.header("Positive Review Word Clouds")
 # select which version of positive wordclouds
 wc_choice_high = st.radio("Select Positive WordCloud Text: ", ["Tokens",'Lemmas'], index=0, horizontal=True)
 wc_choice_high
@@ -36,6 +38,7 @@ else:
     fpath_wc = FPATHS['eda']['tokens-high']
 st.image(fpath_wc)
 
+st.header("Negative Review Word Clouds")
 # select which version of negative wordclouds
 wc_choice_low = st.radio("Select Negative WordCloud Text: ", ["Tokens",'Lemmas'], index=0, horizontal=True)
 wc_choice_low
